85c5e5c8609f6083cb1ed5fe827f66a8d99b4297
Vincent Vanlaer <vincent.vanlaer@kuleuven.be>
2024-01-10 17:05:54 +0100

    Add python helper scripts for benchmarks

--- scripts/plot.py
+++ scripts/plot.py
@@ -1,9 +1,12 @@
+import sys
+from pathlib import Path
+from collections import defaultdict
 import numpy as np
 import matplotlib.pyplot as plt
 from cycler import cycler
 import matplotlib as mpl
 
-def set_style():
+def set_style() -> None:
     mpl.rcParams.update(
         {
             "font.size": 10,
@@ -43,10 +46,33 @@ def set_style():
     )
 
 set_style()
-data = np.loadtxt("a.csv", delimiter=',')
 
-plt.plot(data[:, 0], np.abs(data[:, 1:]), label=["GL2", "GL4", "GL6", "GL8"])
-plt.yscale("log")
-plt.xscale("log")
-plt.legend()
+for test_target in Path("target/criterion").iterdir():
+    if test_target.name == "report":
+        continue
+
+    data = defaultdict(list)
+
+
+    for result in test_target.glob("*/*/result"):
+        error = float(result.read_text())
+        npoints = float(result.parent.name)
+        method = result.parent.parent.name
+        timings = np.genfromtxt(result.parent / "new" / "raw.csv", delimiter=",", names=True)
+        time =  timings[-1]["sample_measured_value"] / timings[-1]["iteration_count"]
+
+        data[method].append((npoints, error, time))
+
+    plt.figure(test_target.name)
+
+    for k, v in data.items():
+        sort = tuple(zip(*sorted(v)))
+        plt.plot(sort[2], np.abs(sort[1]), label=k)
+
+    for i in range(1, 11):
+        plt.axhline(i * sys.float_info.epsilon * 108.40719198544681, ls="--", lw=0.5)
+
+    plt.legend()
+    plt.xscale("log")
+    plt.yscale("log")
 plt.show()
