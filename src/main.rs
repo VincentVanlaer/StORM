a02068eeb746b867e9acf106e6c3d086754792b1
Vincent Vanlaer <vincent.vanlaer@kuleuven.be>
2024-02-06 12:13:00 +0100

    Add option to enable/disable eigenfunctions

--- src/main.rs
+++ src/main.rs
@@ -44,6 +44,8 @@ struct Main {
     n_steps: usize,
     #[arg(long)]
     degree: u32,
+    #[arg(long)]
+    eigenfunctions: bool,
 }
 
 fn main() -> Result<()> {
@@ -107,21 +109,6 @@ fn main() -> Result<()> {
             match solution {
                 Ok(result) => {
                     println!("{:.20} {}", result.freq, result.evals);
-                    let eigenvector = system_matrix(result.freq)?.eigenvector();
-
-                    let (chunks, _) = eigenvector.as_chunks::<4>();
-
-                    let mut vec1 = vec![0.0; chunks.len()];
-                    let mut vec2 = vec![0.0; chunks.len()];
-                    let mut vec3 = vec![0.0; chunks.len()];
-                    let mut vec4 = vec![0.0; chunks.len()];
-
-                    for (i, chunk) in chunks.iter().enumerate() {
-                        vec1[i] = chunk[0];
-                        vec2[i] = chunk[1];
-                        vec3[i] = chunk[2];
-                        vec4[i] = chunk[3];
-                    }
 
                     let group = output.create_group(format!("{i:05}_{m:02}").as_str())?;
 
@@ -130,22 +117,39 @@ fn main() -> Result<()> {
                         .with_data(aview0(&result.freq))
                         .create("freq")?;
 
-                    group
-                        .new_dataset_builder()
-                        .with_data(vec1.as_slice())
-                        .create("y1")?;
-                    group
-                        .new_dataset_builder()
-                        .with_data(vec2.as_slice())
-                        .create("y2")?;
-                    group
-                        .new_dataset_builder()
-                        .with_data(vec3.as_slice())
-                        .create("y3")?;
-                    group
-                        .new_dataset_builder()
-                        .with_data(vec4.as_slice())
-                        .create("y4")?;
+                    if args.eigenfunctions {
+                        let eigenvector = system_matrix(result.freq)?.eigenvector();
+
+                        let (chunks, _) = eigenvector.as_chunks::<4>();
+
+                        let mut vec1 = vec![0.0; chunks.len()];
+                        let mut vec2 = vec![0.0; chunks.len()];
+                        let mut vec3 = vec![0.0; chunks.len()];
+                        let mut vec4 = vec![0.0; chunks.len()];
+
+                        for (i, chunk) in chunks.iter().enumerate() {
+                            vec1[i] = chunk[0];
+                            vec2[i] = chunk[1];
+                            vec3[i] = chunk[2];
+                            vec4[i] = chunk[3];
+                        }
+                        group
+                            .new_dataset_builder()
+                            .with_data(vec1.as_slice())
+                            .create("y1")?;
+                        group
+                            .new_dataset_builder()
+                            .with_data(vec2.as_slice())
+                            .create("y2")?;
+                        group
+                            .new_dataset_builder()
+                            .with_data(vec3.as_slice())
+                            .create("y3")?;
+                        group
+                            .new_dataset_builder()
+                            .with_data(vec4.as_slice())
+                            .create("y4")?;
+                    }
                 }
                 Err(_) => println!("Failed to bracket root"),
             }
