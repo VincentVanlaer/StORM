0c163e8cae75b53d1d85ea04aecf359cf9d30837
Vincent Vanlaer <vincent.vanlaer@kuleuven.be>
2024-08-28 10:57:19 +0200

    Put rust into the flake
    No more surprises

--- flake.nix
+++ flake.nix
@@ -1,14 +1,30 @@
 {
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs = {
+    nixpkgs.url = "nixpkgs";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
-  outputs = { nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [ cmake gcc pkg-config gfortran (python3.withPackages (p: [ p.numpy p.scipy p.matplotlib p.mypy p.pyqt6 p.h5py ])) gnuplot openssl maxima];
-        };
-      });
+  outputs = { nixpkgs, fenix, ... }:
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in
+  {
+    devShells.${system}.default = pkgs.mkShell {
+      nativeBuildInputs = with pkgs; [
+        cmake
+        gcc
+        pkg-config
+        gfortran
+        (python3.withPackages (p: [ p.numpy p.scipy p.matplotlib p.mypy p.pyqt6 p.h5py ]))
+        gnuplot
+        openssl
+        maxima
+        (fenix.packages.${system}.latest.withComponents ["cargo" "clippy" "rustc" "rustfmt" "rust-analyzer" "rust-src" "rust-std"])
+      ];
+    };
+  };
 }
