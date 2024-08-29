ff1711866c06cc0ba0be107a157af19de8e8994f
Vincent Vanlaer <vincent.vanlaer@kuleuven.be>
2024-08-29 17:34:09 +0200

    Revert "Switch to logarithmic coordinates"
    This reverts commit 82bd42804aba640582f490dd403b92b849b15847.

--- src/system/adiabatic.rs
+++ src/system/adiabatic.rs
@@ -157,9 +157,11 @@ impl Iterator for ModelPointsIterator<'_> {
         let upper = self.model.components[self.pos + 1];
         let upper_a = add_frequency(upper);
 
-        let intercept = lower_a
-            + (upper_a - lower_a) * ((self.subpos as f64 + 0.5) / (self.total_subpos as f64));
-        let slope = (upper_a - lower_a) * (1.0 / (self.total_subpos as f64));
+        let intercept = lower_a * (1.0 / lower.x)
+            + (upper_a * (1.0 / upper.x) - lower_a * (1.0 / lower.x))
+                * ((self.subpos as f64 + 0.5) / (self.total_subpos as f64));
+        let slope = (upper_a * (1.0 / upper.x) - lower_a * (1.0 / lower.x))
+            * (1.0 / (self.total_subpos as f64));
 
         let delta = (upper.x - lower.x) / (self.total_subpos as f64);
         let sublower = lower.x + delta * (self.subpos as f64);
@@ -172,7 +174,7 @@ impl Iterator for ModelPointsIterator<'_> {
             self.subpos = 0;
         }
 
-        Some((subupper.ln() - sublower.ln(), slope, intercept))
+        Some((subupper - sublower, slope, intercept))
     }
 }
 
