adc4534882d21214e4df8de04a114239d34999bd
Vincent Vanlaer <vincent.vanlaer@kuleuven.be>
2024-08-30 15:20:12 +0200

    Add inspection scripts

---
+++ scripts/inspect-balanced.py
@@ -0,0 +1,35 @@
+import h5py
+import matplotlib.pyplot as plt
+
+data = h5py.File("test-data/balanced-inspect.hdf5")
+
+plt.plot(data["scan"]["freq"][:], data["scan"]["value"][:])
+
+for k in data["sol"].keys():
+    evals = data["sol"][k].attrs["evals"]
+    for j in range(evals):
+        j = f"eval_{j}"
+        d = data["sol"][k][j]
+
+        lims = [d.attrs["next"], d.attrs["lower"], d.attrs["upper"]]
+
+        fig, (ax1, ax2) = plt.subplots(nrows=2, ncols=1, sharex=True)
+        ax1.plot(d["freq"][:], d["value"][:])
+        ax1.axvline(d.attrs["next"], label="next")
+        ax1.axvline(d.attrs["lower"], c="red", ls="--", label="lower")
+        ax1.axvline(d.attrs["upper"], c="green", ls="-.", label="upper")
+        if "previous" in d.attrs:
+            ax1.axvline(d.attrs["previous"], c="pink", ls="-.", label="previous")
+            lims.append(d.attrs["previous"])
+        ax1.axhline(0, ls="--")
+        ax1.axhline(d.attrs["offset"], ls="-.", label=f"offset ({d.attrs['offset']})")
+        ax1.text
+        ax1.legend()
+
+        delta = max(lims) - min(lims)
+
+        ax1.set_xlim((min(lims) - delta * 0.1, max(lims) + delta * 0.1))
+
+        ax2.plot(d["freq"][:-1], (d["value"][1:] - d["value"][:-1]) / (d["freq"][1:] - d["freq"][:-1]))
+
+plt.show()
---
+++ scripts/inspect-brent.py
@@ -0,0 +1,32 @@
+import h5py
+import matplotlib.pyplot as plt
+
+data = h5py.File("test-data/brent-inspect.hdf5")
+
+plt.plot(data["scan"]["freq"][:], data["scan"]["value"][:])
+
+for k in data["sol"].keys():
+    evals = data["sol"][k].attrs["evals"]
+    for j in range(evals):
+        j = f"eval_{j}"
+        d = data["sol"][k][j]
+        fig, (ax1, ax2) = plt.subplots(nrows=2, ncols=1, sharex=True)
+        ax1.plot(d["freq"][:], d["value"][:])
+        ax1
+        ax1.axvline(d.attrs["next"], label=f"next ({d.attrs['method']})")
+        ax1.axvline(d.attrs["current"], c="red", ls="--", label="current")
+        ax1.axvline(d.attrs["previous"], c="green", ls="-.", label="previous")
+        ax1.axvline(d.attrs["counterpoint"], c="pink", ls="--", label="counterpoint")
+        ax1.axhline(0, ls="--")
+        ax1.text
+        ax1.legend()
+
+        lims = [d.attrs["next"], d.attrs["previous"], d.attrs["current"], d.attrs["counterpoint"]]
+
+        delta = max(lims) - min(lims)
+
+        ax1.set_xlim((min(lims) - delta * 0.1, max(lims) + delta * 0.1))
+
+        ax2.plot(d["freq"][:-1], (d["value"][1:] - d["value"][:-1]) / (d["freq"][1:] - d["freq"][:-1]))
+
+plt.show()
