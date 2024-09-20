04d46000f2bb1e9167e7a6d5e22620249f1341c3
Vincent Vanlaer <vincent.vanlaer@kuleuven.be>
2024-09-20 15:08:07 +0200

    build: add cargo-show-asm to dev env

--- flake.nix
+++ flake.nix
@@ -7,24 +7,59 @@
     };
   };
 
-  outputs = { nixpkgs, fenix, ... }:
-  let
+  outputs = {
+    nixpkgs,
+    fenix,
+    ...
+  }: let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
-  in
-  {
+    toolchain = fenix.packages.${system}.latest.withComponents ["cargo" "clippy" "rustc" "rustfmt" "rust-analyzer" "rust-src" "rust-std"];
+    buildRustPackage =
+      (pkgs.makeRustPlatform {
+        cargo = toolchain;
+        rustc = toolchain;
+      })
+      .buildRustPackage;
+    cargo-show-asm = buildRustPackage {
+      pname = "cargo-show-asm";
+      version = "0.2.36";
+
+      src = pkgs.fetchCrate {
+        pname = "cargo-show-asm";
+        version = "0.2.36";
+        hash = "sha256-Ptv8txt7YXPh1QvFwsoRbBQgeLBGn6iVqst1GHU+JJw=";
+      };
+
+      cargoHash = "sha256-GkhFbRhEJ+7ikqkNPydKx3Ty8KRsGts5UnwA8bl8Po8=";
+      buildFeatures = ["disasm"];
+
+      nativeBuildInputs = [
+        pkgs.installShellFiles
+      ];
+
+      postInstall = ''
+        installShellCompletion --cmd cargo-asm \
+          --bash <($out/bin/cargo-asm --bpaf-complete-style-bash) \
+          --fish <($out/bin/cargo-asm --bpaf-complete-style-fish) \
+          --zsh  <($out/bin/cargo-asm --bpaf-complete-style-zsh)
+      '';
+    };
+  in {
     devShells.${system}.default = pkgs.mkShell {
       nativeBuildInputs = with pkgs; [
         cmake
         gcc
         pkg-config
         gfortran
-        (python3.withPackages (p: [ p.numpy p.scipy p.matplotlib p.mypy p.pyqt6 p.h5py ]))
+        (python3.withPackages (p: [p.numpy p.scipy p.matplotlib p.mypy p.pyqt6 p.h5py]))
         gnuplot
         openssl.dev
         hdf5.dev
         maxima
-        (fenix.packages.${system}.latest.withComponents ["cargo" "clippy" "rustc" "rustfmt" "rust-analyzer" "rust-src" "rust-std"])
+        toolchain
+        cargo-show-asm
+        llvm
       ];
     };
   };
