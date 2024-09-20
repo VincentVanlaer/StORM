d38734a5e91e6dc40db29c26a2744d64b682a1cc
Vincent Vanlaer <vincent.vanlaer@kuleuven.be>
2024-09-20 10:43:27 +0200

    feat: compare StORM and GYRE output

--- scripts/plot-gyre-benches.py
+++ scripts/plot-gyre-benches.py
@@ -1,6 +1,9 @@
 import matplotlib.pyplot as plt
+import h5py
 import json
 import numpy as np
+
+plt.rcParams["figure.constrained_layout.use"] = True
 
 DIFF_SCHEMES = [ "COLLOC_GL2", "COLLOC_GL4", "COLLOC_GL6", "MAGNUS_GL2", "MAGNUS_GL4", "MAGNUS_GL6"]
 STORM_SCHEMES = [ "colloc2", "colloc4", "colloc6", "magnus2", "magnus4", "magnus6" ]
@@ -9,9 +12,24 @@ results_block = []
 results_band = []
 results_storm = []
 
+freqs = {}
+freqs_storm = {}
+
 for d in DIFF_SCHEMES:
-    block = json.load(open(f"test-data/generated/{d}_BLOCK.json"))["results"][0]["mean"]
-    band = json.load(open(f"test-data/generated/{d}_BAND.json"))["results"][0]["mean"]
+    try:
+        block = json.load(open(f"test-data/generated/{d}_BLOCK.json"))["results"][0]["mean"]
+    except (FileNotFoundError, json.decoder.JSONDecodeError):
+        block = np.nan
+
+    try:
+        band = json.load(open(f"test-data/generated/{d}_BAND.json"))["results"][0]["mean"]
+    except (FileNotFoundError, json.decoder.JSONDecodeError):
+        band = np.nan
+
+    freq_block = h5py.File(f"test-data/generated/{d}_BLOCK/summary.h5")["freq"]["re"][:]
+    freq_band = h5py.File(f"test-data/generated/{d}_BAND/summary.h5")["freq"]["re"][:]
+
+    freqs[f"{d}_BAND"] = freq_band
 
     results_block.append(block)
     results_band.append(band)
@@ -19,6 +37,18 @@ for d in DIFF_SCHEMES:
 for d in STORM_SCHEMES:
     try:
         storm = json.load(open(f"test-data/generated/{d}_storm.json"))["results"][0]["mean"]
+        storm_summary = h5py.File(f"test-data/generated/{d}_storm/summary.hdf5")
+
+        storm_freqs_tmp = []
+        for g in storm_summary:
+            try:
+                storm_freqs_tmp.append(storm_summary[g].attrs['freq'])
+            except KeyError:
+                pass
+
+        storm_summary.close()
+
+        freqs_storm[f'{d}_storm'] = storm_freqs_tmp
     except FileNotFoundError:
         storm = np.nan
 
@@ -40,3 +70,19 @@ plt.xticks(x + width, DIFF_SCHEMES, rotation=45)
 plt.legend()
 plt.title("Comparison of GYRE and StORM performance for various difference schemes. The stellar model is a 6200 point MESA model of a beta-Cepheid star (HD 192575). Input scan parameters are 1 to 25 dimless angular frequency with 25 scan points, radial modes. In total, 19 frequencies are extracted up to machine precision. GYRE is using the Brent bracketing algorithm, while StORM is using the custom Balanced algorithm. All regridding has been disabled, and GYRE has been forced to use the full set of equations, rather than the second-order reduced equations. Ran on a single core of an i7-1185G7.", wrap=True, fontsize=8, pad=10, loc='left')
 plt.savefig("test-data/generated/gyre-performance.pdf")
+plt.close()
+
+ref = freqs.pop("COLLOC_GL6_BAND")
+
+for k, v in freqs.items():
+    plt.plot(v / ref - 1.0, ".", label=k)
+
+plt.gca().set_prop_cycle(None)
+
+for k, v in freqs_storm.items():
+    plt.plot(v / ref - 1.0, "x", label=k, markersize=4)
+
+plt.xlabel("Frequency (dimensionless)")
+plt.ylabel("Relative difference with GYRE COLLOC_GL6")
+plt.legend()
+plt.savefig("test-data/generated/gyre-compare.pdf")
