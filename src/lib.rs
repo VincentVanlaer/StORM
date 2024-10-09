af41594ec6d422dabcc4c9cf0191ac42332b4ec3
Vincent Vanlaer <vincent.vanlaer@kuleuven.be>
2024-10-09 14:19:18 +0200

    doc: add example to main page

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,37 @@
 //! Stellar Oscillations with Rotation and Magnetism
+//!
+//! ```
+//! # use storm::bracket::Precision;
+//! # use storm::dynamic_interface::{DifferenceSchemes, MultipleShooting};
+//! # use storm::model::StellarModel;
+//! # use storm::system::adiabatic::{GridScale, Rotating1D};
+//! let model = StellarModel::from_gsm("model.hdf5").unwrap();
+//!
+//! let lower = 1.0;
+//! let upper = 25.0;
+//! let n_steps = 25;
+//! let degree = 1;
+//! let order = 0;
+//!
+//! let system = Rotating1D::from_model(&model, degree, order);
+//! let grid = GridScale { scale: 0 };
+//! let solver = MultipleShooting::new(&system, DifferenceSchemes::Colloc2, &grid);
+//!
+//! let solutions: Vec<_> = solver
+//!     .scan_and_optimize(
+//!         linspace(lower, upper, n_steps),
+//!         Precision::Relative(0.),
+//!     )
+//!     .collect();
+//!
+//! for s in solutions {
+//!     println!("{}", s.root);
+//! }
+//! #
+//! # fn linspace(lower: f64, upper: f64, n: usize) -> impl Iterator<Item = f64> {
+//! #     (0..n).map(move |x| lower + (upper - lower) * (x as f64) / ((n - 1) as f64))
+//! # }
+//! ```
 #![feature(generic_const_exprs)]
 #![feature(const_option)]
 #![feature(iter_map_windows)]
