VincentVanlaer/StORM

commit 0d4409c51f5422ac57996f3ac18f5e41c51b7613

performance: use column elimination of transposed matrix
This is the most efficient in the case of column major ordering

src/system/adiabatic.rs
@@ -48,23 +48,23 @@ impl Rotating1D {
 
         for (i, component) in components.iter_mut().enumerate() {
             component.a[0][0] = v_gamma[i] - 1.0 - ell;
-            component.a[1][0] = -v_gamma[i];
-            component.a[2][0] = 0.0;
-            component.a[3][0] = 0.0;
-
-            component.a[0][1] = -a_star[i];
-            component.a[1][1] = a_star[i] - u[i] + 3. - ell;
-            component.a[2][1] = 0.;
-            component.a[3][1] = -1.;
-
+            component.a[0][1] = -v_gamma[i];
             component.a[0][2] = 0.0;
-            component.a[1][2] = 0.0;
-            component.a[2][2] = 3. - u[i] - ell;
-            component.a[3][2] = 1.;
+            component.a[0][3] = 0.0;
 
-            component.a[0][3] = u[i] * a_star[i];
-            component.a[1][3] = u[i] * v_gamma[i];
-            component.a[2][3] = ell * (ell + 1.);
+            component.a[1][0] = -a_star[i];
+            component.a[1][1] = a_star[i] - u[i] + 3. - ell;
+            component.a[1][2] = 0.;
+            component.a[1][3] = -1.;
+
+            component.a[2][0] = 0.0;
+            component.a[2][1] = 0.0;
+            component.a[2][2] = 3. - u[i] - ell;
+            component.a[2][3] = 1.;
+
+            component.a[3][0] = u[i] * a_star[i];
+            component.a[3][1] = u[i] * v_gamma[i];
+            component.a[3][2] = ell * (ell + 1.);
             component.a[3][3] = -u[i] + 2. - ell;
 
             component.rot = value.rot[i];
@@ -138,15 +138,15 @@ impl Iterator for ModelPointsIterator<'_> {
             }
 
             a[0][0] += -lambda * rel_rot;
-            a[1][0] += lambda.powi(2) / (omega_rsq * point.c1);
-            a[2][0] += lambda.powi(2) / (omega_rsq * point.c1);
+            a[0][1] += lambda.powi(2) / (omega_rsq * point.c1);
+            a[0][2] += lambda.powi(2) / (omega_rsq * point.c1);
 
-            a[0][1] += point.c1
+            a[1][0] += point.c1
                 * (omega - m * point.rot).powi(2)
                 * (1. - (2. * m * point.rot).powi(2) / omega_rsq);
 
             a[1][1] += lambda * rel_rot;
-            a[2][1] += lambda * rel_rot;
+            a[1][2] += lambda * rel_rot;
 
             a
         };
@@ -242,7 +242,7 @@ impl Moments<f64, ModelGrid, 4, 4> for Rotating1D {
 }
 
 impl Boundary<f64, 4, 2> for Rotating1D {
-    fn inner_boundary(&self, omega: f64) -> Matrix<f64, 2, 4> {
+    fn inner_boundary(&self, omega: f64) -> Matrix<f64, 4, 2> {
         let l = self.ell;
         let lambda = l * (l + 1.);
         let m = self.m;
@@ -262,21 +262,19 @@ impl Boundary<f64, 4, 2> for Rotating1D {
                 self.components[0].c1
                     * (omega - m * rot).powi(2)
                     * (1. - (2. * m * rot).powi(2) / omega_rsq),
-                0.,
+                lambda * rel_rot - self.ell,
+                lambda * rel_rot - self.ell,
+                0.
             ],
-            [lambda * rel_rot - self.ell, 0.],
-            [lambda * rel_rot - self.ell, self.ell],
-            [0., -1.],
+            [0., 0., self.ell, 1.]
         ]
         .into()
     }
 
-    fn outer_boundary(&self, _frequency: f64) -> Matrix<f64, 2, 4> {
+    fn outer_boundary(&self, _frequency: f64) -> Matrix<f64, 4, 2> {
         [
-            [1., self.u_upper],
-            [-1., 0.],
-            [0., self.ell + 1.0],
-            [0., 1.],
+            [1., -1., 0., 0.],
+            [self.u_upper, 0., self.ell + 1., 1.]
         ]
         .into()
     }

src/solver.rs
@@ -102,7 +102,7 @@ where
 
     for i in 0..N_INNER {
         for j in 0..N {
-            bands[i][j] = inner_boundary[j][i];
+            bands[i][j] = inner_boundary[i][j];
         }
     }
 
@@ -110,8 +110,8 @@ where
     for (nstep, step) in iterator.map(|x| stepper.step(x)).enumerate() {
         for i in 0..N {
             for j in 0..N {
-                bands[i + 2][j] = step.left[j][i];
-                bands[i + 2][j + N] = step.right[j][i];
+                bands[i + 2][j] = step.left[i][j];
+                bands[i + 2][j + N] = step.right[i][j];
             }
         }
 
@@ -124,10 +124,6 @@ where
                     max_idx = i;
                     max_val = bands[i][k];
                 }
-            }
-
-            if max_val == 0. {
-                panic!("Bail at {nstep}");
             }
 
             if max_idx != k {
@@ -160,7 +156,7 @@ where
     // Outer boundary
     for i in 0..(N - N_INNER) {
         for j in 0..N {
-            bands[N_INNER + i][j] = outer_boundary[j][i];
+            bands[N_INNER + i][j] = outer_boundary[i][j];
         }
     }
 
@@ -173,10 +169,6 @@ where
                 max_idx = i;
                 max_val = bands[i][k];
             }
-        }
-
-        if max_val == 0. {
-            panic!("Bail at end");
         }
 
         if max_idx != k {
@@ -236,18 +228,18 @@ where
 
     for j in 0..N {
         for k in 0..N_INNER {
-            storage[j][kl + ku + k - j] = inner_boundary[j][k];
+            storage[j][kl + ku + k - j] = inner_boundary[k][j];
         }
         for k in 0..(N - N_INNER) {
-            storage[alen - N + j][kl + ku + N - j - (N - N_INNER) + k] = outer_boundary[j][k];
+            storage[alen - N + j][kl + ku + N - j - (N - N_INNER) + k] = outer_boundary[k][j];
         }
     }
 
     for (i, step) in iterator.map(|x| stepper.step(x)).enumerate() {
         for j in 0..N {
             for k in 0..N {
-                storage[i * N + j][kl + ku + k - j + N_INNER] = step.left[j][k];
-                storage[(i + 1) * N + j][kl + ku + k - j - N + N_INNER] = step.right[j][k];
+                storage[i * N + j][kl + ku + k - j + N_INNER] = step.left[k][j];
+                storage[(i + 1) * N + j][kl + ku + k - j - N + N_INNER] = step.right[k][j];
             }
         }
     }

src/system.rs
@@ -10,8 +10,8 @@ pub(crate) trait Moments<T: Float, G: ?Sized, const N: usize, const ORDER: usize
 }
 
 pub(crate) trait Boundary<T: Float, const N: usize, const N_INNER: usize> {
-    fn inner_boundary(&self, frequency: f64) -> Matrix<T, N_INNER, N>;
-    fn outer_boundary(&self, _frequency: f64) -> Matrix<T, { N - N_INNER }, N>;
+    fn inner_boundary(&self, frequency: f64) -> Matrix<T, N, N_INNER>;
+    fn outer_boundary(&self, _frequency: f64) -> Matrix<T, N, { N - N_INNER }>;
 }
 
 #[allow(private_bounds)]

commit 702cf2b853d6fa0759cafc03d8d29983b45d223f

fix: inner boundary condition wrong sign
Was accidentally broken by 0d4409c5

src/system/adiabatic.rs
@@ -318,7 +318,7 @@ impl Boundary<f64, 4, 2> for Rotating1D {
                 lambda * rel_rot - self.ell,
                 0.,
             ],
-            [0., 0., self.ell, 1.],
+            [0., 0., self.ell, -1.],
         ]
         .into()
     }