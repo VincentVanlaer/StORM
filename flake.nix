6d3f07aae2c71c6bf8c3ecc1387ac32b201f49af
Vincent Vanlaer <vincent.vanlaer@kuleuven.be>
2025-02-12 18:32:10 +0100

    cleanup dependencies in flake.nix

--- flake.nix
+++ flake.nix
@@ -16,52 +16,16 @@
     pkgs = nixpkgs.legacyPackages.${system};
     f = fenix.packages.${system};
     toolchain = f.combine [f.latest.cargo f.latest.clippy f.latest.rustc f.latest.rustfmt f.latest.rust-analyzer f.latest.rust-src f.latest.rust-std f.targets.x86_64-unknown-linux-musl.latest.rust-std f.latest.miri];
-    buildRustPackage =
-      (pkgs.makeRustPlatform {
-        cargo = toolchain;
-        rustc = toolchain;
-      })
-      .buildRustPackage;
-    cargo-show-asm = buildRustPackage {
-      pname = "cargo-show-asm";
-      version = "0.2.36";
-
-      src = pkgs.fetchCrate {
-        pname = "cargo-show-asm";
-        version = "0.2.36";
-        hash = "sha256-Ptv8txt7YXPh1QvFwsoRbBQgeLBGn6iVqst1GHU+JJw=";
-      };
-
-      cargoHash = "sha256-GkhFbRhEJ+7ikqkNPydKx3Ty8KRsGts5UnwA8bl8Po8=";
-      buildFeatures = ["disasm"];
-
-      nativeBuildInputs = [
-        pkgs.installShellFiles
-      ];
-
-      postInstall = ''
-        installShellCompletion --cmd cargo-asm \
-          --bash <($out/bin/cargo-asm --bpaf-complete-style-bash) \
-          --fish <($out/bin/cargo-asm --bpaf-complete-style-fish) \
-          --zsh  <($out/bin/cargo-asm --bpaf-complete-style-zsh)
-      '';
-    };
   in {
     devShells.${system}.default = pkgs.mkShell {
       nativeBuildInputs = with pkgs; [
         cmake
         pkg-config
-        openssl.dev
-        openblasCompat.dev
         (python3.withPackages (p: [p.numpy p.scipy p.matplotlib p.mypy p.pyqt6 p.h5py p.tqdm]))
-        gnuplot
         hdf5.dev
         maxima
         toolchain
-        cargo-show-asm
-        cargo-watch
         bacon
-        llvm
         nodePackages.browser-sync
       ];
 
